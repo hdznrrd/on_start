5fa693e5afa93c9aee5e5b0c84cf92ab4c0ef98c
hdznrrd <hadez@nrrd.de>
2012-06-03 13:21:57 +0200

    initial commit

---
+++ on_start.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+#
+# On Start
+# Copyright (C) 2007 Gregor Jehle <gjehle@gmail.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject
+# to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# 
+
+package ON_START;
+
+my $mcps_masterkey = '*************';
+my $fish_inipw = '***********';
+IRC::register("on start","0.1","","");
+IRC::add_timeout_handler(300,'ON_START::on_start');
+IRC::print("on start. loaded");
+
+sub on_start
+{
+	IRC::command("/masterkey $mcps_masterkey");
+	IRC::command("/fishpw $fish_inipw");
+}
+
+
+
+
